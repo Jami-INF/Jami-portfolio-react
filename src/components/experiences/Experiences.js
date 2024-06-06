4ca6f671179cdca45caf9d0a9963a547c83374de
infantej <>
2024-06-06 15:48:08 +0200

    UPD : contenu du site (Expériences pro)

--- src/components/experiences/Experiences.js
+++ src/components/experiences/Experiences.js
@@ -1,8 +1,10 @@
-import {Fade} from "react-awesome-reveal";
+import { Fade } from "react-awesome-reveal";
 import React from "react";
 import Study from "./Study";
 import Work from "./Work";
 import NTLogo from "../../assets/logos/NT-logo.png";
+import FLDLogo from "../../assets/logos/FLD-logo.png";
+import EFFLogo from "../../assets/logos/EFF-logo.png";
 
 const Experiences = () => {
     return (
@@ -34,20 +36,32 @@ const Experiences = () => {
                 <Fade>
                     <ol className="list-none p-0">
                         <Work
+                            logoSrc={EFFLogo}
+                            title="Développeur WEB - EMIL FREY FRANCE (P.B.O)"
+                            date="2024-now"
+                            description="Développement et déploiement d'un site de vente de pièces automobile en ligne pour plusieurs clients"
+                        />
+                        <Work
+                            logoSrc={FLDLogo}
+                            title="Développeur WEB - FLAURAUD - AURILIS GROUP"
+                            date="2023-2024"
+                            description="Développement sur le site de vente de pièces automobile en ligne"
+                        />
+                        <Work
                             logoSrc={NTLogo}
-                            title="Alternance - Développeur WEB"
+                            title="Alternance - Développeur WEB - NUMTECH"
                             date="2022-2023"
                             description="Développement sur différents projets internes et pour des clients orienté Web. Languages : PHP, JS, HTML, CSS"
                         />
                         <Work
                             logoSrc={NTLogo}
-                            title="Job d'été chez Numtech - Assistant Ingénieur"
+                            title="Job d'été chez Numtech - Assistant Ingénieur - NUMTECH"
                             date="2022"
                             description="Développement sur différents projets internes et pour des clients orienté Web. Languages : PHP, JS, HTML, CSS. Réalisations de diverses tâches pour un client (Data treatment). Continuation et aboutissement du développement de l'outil de gestion interne commencés précédemment lors du stage"
                         />
                         <Work
                             logoSrc={NTLogo}
-                            title="Stage de 10 semaines chez Numtech"
+                            title="Stage de 10 semaines - Développeur WEB - NUMTECH"
                             date="2022"
                             description="Développement d’un outil de gestion interne. Analyse de l'existant, analyse des besoins, développement en PHP utilisant le modèle MVC, JS, HTML, CSS. Dans ce stage, ma mission était de créer ou reprendre le développement d’un outil de gestion interne..."
                         />
